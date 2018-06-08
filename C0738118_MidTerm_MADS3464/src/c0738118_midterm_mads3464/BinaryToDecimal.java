21b0a2ae56ba23c80bf4f5137707e4572825647f
Gagandeep1101 <gagansandhu32@yahoo.com>
2018-06-07 20:02:55 -0400

    BinaryToDecimal file done

--- C0738118_MidTerm_MADS3464/src/c0738118_midterm_mads3464/BinaryToDecimal.java
+++ C0738118_MidTerm_MADS3464/src/c0738118_midterm_mads3464/BinaryToDecimal.java
@@ -9,16 +9,32 @@ package c0738118_midterm_mads3464;
  *
  * @author Dhillon Huni
  */
-public class C0738118_MidTerm_MADS3464 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-    /*{
-        LambtonStringTools lst = new LambtonStringTools();
-        lst.reverse("lambton");*/
-    
+
+
+   public class BinaryToDecimal {
+
+	public int binaryToDecimal(int binary)
+	{
+	int decimal = 0;
+	int power = 0;
+		
+	while(true){
+	if(binary == 0){
+	break;
+	} 
+	else {
+	int temp = binary%10;
+	decimal += temp*Math.pow(2, power);
+	binary = binary/10;
+	power++;
+	}
+	}
+	return decimal;
+	}
+	
+	public static void main(String args[])
+	{
 		BinaryToDecimal obj = new BinaryToDecimal();
 		
 		System.out.println("Decimal of 1101 : "+obj.binaryToDecimal(1101));
